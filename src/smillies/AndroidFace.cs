16c5ca3a41243701b75ab1b2118fc8e644932e0f
xize <glucassen@live.nl>
2016-06-30 00:35:53 +0200

    add android face emotion

---
+++ src/smillies/AndroidFace.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emoji_keyboard.src.smillies
+{
+    class AndroidFace : Smilley
+    {
+        public override string getCharacter()
+        {
+            return "👾";
+        }
+
+        public override Image getSmilleyImage()
+        {
+            return Properties.Resources.android;
+        }
+
+        public override SmilleyType getType()
+        {
+            return SmilleyType.ANDROID_FACE;
+        }
+    }
+}
