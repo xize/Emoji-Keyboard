64bea0cf6b28b1df66b52c5ec133550632c4a88d
xize <glucassen@live.nl>
2016-06-29 20:36:02 +0200

    add Happy2 emotion

--- src/smillies/SmilleyType.cs
+++ src/smillies/SmilleyType.cs
@@ -15,6 +15,7 @@ namespace emoji_keyboard.src.smillies
         public static SmilleyType HAPPY = new SmilleyType("happy", new Happy());
         public static SmilleyType GRINIC_FACE = new SmilleyType("grinic_face", new GrinicFace());
         public static SmilleyType JOY_FACE = new SmilleyType("joy_face", new JoyFace());
+        public static SmilleyType HAPPY_2 = new SmilleyType("happy_2", new Happy2());
         
         private string name;
         private Smilley smilley;
