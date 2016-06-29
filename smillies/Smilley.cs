d6fb80bc9dfb8df6ce7b6318db55c00acdc1942e
xize <glucassen@live.nl>
2016-06-29 20:16:41 +0200

    add basic hierarchy for emojies

---
+++ smillies/Smilley.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace emoji_keyboard.src.smillies
+{
+    abstract class Smilley
+    {
+
+        private Label smilley;
+
+       public abstract string getName();
+
+        public abstract SmilleyType getType();
+
+        public abstract Image getSmilleyImage();
+
+        public abstract string getCharacter();
+
+        public Image getSmallSmilley()
+        {
+            return new Bitmap(getSmilleyImage(), 23, 23);
+        }
+        
+        public Label getFormSmilley()
+        {
+            if(smilley == null)
+            {
+                this.smilley = new Label();
+                smilley.Height = 23;
+                smilley.Width = 23;
+                smilley.Image = getSmallSmilley();
+            }
+            return smilley;
+        }
+
+    }
+}
---
+++ smillies/SmilleyType.cs
@@ -0,0 +1,42 @@
+﻿using emoji_keyboard.smillies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emoji_keyboard.src.smillies
+{
+    class SmilleyType
+    {
+
+        private static Dictionary<string, SmilleyType> smillies = new Dictionary<string, SmilleyType>();
+
+        public static SmilleyType HAPPY = new SmilleyType("happy", new Happy());
+        public static SmilleyType GRINIC_FACE = new SmilleyType("grinic_face", new GrinicFace());
+        
+        private string name;
+        private Smilley smilley;
+
+        private SmilleyType(string name, Smilley smilley)
+        {
+            this.name = name;
+            this.smilley = smilley;
+            if(!smillies.ContainsKey(smilley.getCharacter()))
+            {
+                smillies.Add(smilley.getCharacter(), this);
+            }
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public static KeyValuePair<string, SmilleyType>[] values()
+        {
+            return smillies.ToArray();
+        }
+
+    }
+}
