ad109bfab1684541647e79d117feca78a68c8af1
xize <glucassen@live.nl>
2016-06-29 23:29:03 +0200

    add disappointed face emotion

---
+++ src/smillies/DisappointedFace.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emoji_keyboard.src.smillies
+{
+    class DisappointedFace : Smilley
+    {
+        public override string getCharacter()
+        {
+            return "😞";
+        }
+
+        public override Image getSmilleyImage()
+        {
+            return Properties.Resources.disappointed_face;
+        }
+
+        public override SmilleyType getType()
+        {
+            return SmilleyType.DISAPPOINTED_FACE;
+        }
+    }
+}
